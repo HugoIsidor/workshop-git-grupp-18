bcd344ba4f15f37ff2a6aa79871643454fd45e53
rydis01 <carl.rydengard@hotmail.com>
2026-03-27 09:30:02 +0100

    Kort 2
    Menu: add 'Add' + choice prompt

--- App.java
+++ App.java
@@ -1,5 +1,7 @@
 public class App {
     static void run() {
-        System.out.println("Calculateor v0.1");
+        System.out.println("1) Add(\n0) Exit");
+        System.out.println("Choice: ");
+        String choice = new java.util.Scanner(System.in).nextLine();
     }
 }
