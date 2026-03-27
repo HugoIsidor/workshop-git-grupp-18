fdb8935616077d812cb94f95882c68ec04285073
Johnsson-G <gustav.johnsson177@gmail.com>
2026-03-27 10:38:35 +0100

    Update App.java

--- App.java
+++ App.java
@@ -1,6 +1,8 @@
 public class App {
     static void run() {
 
+        //Smple console calculator
+
         System.out.println("(1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Exit");
         if ("2".equals(choice)) System.out.println(Calculator.sub(a, b));
 
@@ -10,8 +12,8 @@ public class App {
         double a = Double.parseDouble(new java.util.Scanner(System.in).nextLine());
         double b = Double.parseDouble(new java.util.Scanner(System.in).nextLine());
         System.out.println(Calculator.add(a,b));
+        if("3".equals(choise)) System.out.println(Calculator.mul(a, b));
     }
-
-    if("3".equals(choise)) System.out.println(Calculator.mul(a, b));
+    
 
 }
