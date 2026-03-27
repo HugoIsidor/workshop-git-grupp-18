6430182b5298ea3e1ba3be6ec65712ce896cb955
rydis01 <carl.rydengard@hotmail.com>
2026-03-27 10:35:16 +0100

    Kort 18
    Handle choice 4 using div()

--- App.java
+++ App.java
@@ -2,7 +2,6 @@ public class App {
     static void run() {
 
         System.out.println("(1) Add\n2) Subtract\n3) Multiply\n4) Divide\n0) Exit");
-        if ("2".equals(choice)) System.out.println(Calculator.sub(a, b));
 
         System.out.println("Choice: ");
         String choice = new java.util.Scanner(System.in).nextLine();
@@ -10,5 +9,7 @@ public class App {
         double a = Double.parseDouble(new java.util.Scanner(System.in).nextLine());
         double b = Double.parseDouble(new java.util.Scanner(System.in).nextLine());
         System.out.println(Calculator.add(a,b));
+        if ("2".equals(choice)) System.out.println(Calculator.sub(a, b));
+        if ("4".equals(choice)) System.out.println(Calculator.div(a, b));
     }
 }
